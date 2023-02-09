6bdd935eef3159ccc8e250761a61164c57379eb5
Bruno Lopes <srlopes.bruno@gmail.com>
2023-02-09 14:00:37 -0300

    att

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const psicologosController = require("../controllers/psicologosController");
+const pacienteController = require("../controllers/pacienteController");
 const requestLog = require("../middlewares/requestLog");
 const bloqueio = require("../middlewares/bloqueio");
 const routes = express.Router();
@@ -14,11 +15,11 @@ routes.post("/psicologos", psicologosController.cadastrarPsicologo);
 routes.delete("/psicologos/:id", psicologosController.deletarPsicologo);
 routes.put("/psicologos/:id", psicologosController.atualizarPsicologo);
 
-routes.get("/pacientes", requestLog, psicologosController.listarPsicologos);
-routes.get("/pacientes/:id", psicologosController.listarPsicologosId);
-routes.post("/pacientes", psicologosController.cadastrarPsicologo);
-routes.delete("/pacientes/:id", psicologosController.deletarPsicologo);
-routes.put("/pacientes/:id", psicologosController.atualizarPsicologo);
+routes.get("/pacientes", requestLog, pacienteController.listarPacientes);
+routes.get("/pacientes/:id", pacienteController.listaPacientes);
+routes.post("/pacientes", pacienteController.cadastrarPaciente);
+routes.delete("/pacientes/:id", pacienteController.deletePaciente);
+routes.put("/pacientes/:id", pacienteController.atualizarPsicologo);
 
 routes.post("/atendimento", requestLog, bloqueio, psicologosController.listarPsicologos);
 
